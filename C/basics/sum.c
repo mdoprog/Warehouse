153d783a088c5e09d18eaac2a02d95724ced7142
Marco <oliveira.demarco@gmail.com>
2020-09-15 11:37:19 -0300

    File names corrected

--- C/basics/sum.c
+++ C/basics/sum.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-//std = Standard (Padrão)
-//io = input/output (Entrada/Saída)
 
-int main(){
-	//Declaração de variáveis
-	int num1, num2, soma, multiplicacao;
+int main() {
+	//Declarando variáveis
+	int num1, num2, soma;
 
-	//Entrada
-	printf("Informe o primeiro número: ");
-	fflush(stdout);
-	scanf("%d", &num1);
+	//Entradas
+	printf ("Informe o primeiro número: ");
+	fflush (stdout);
+	scanf ("%d", &num1);
 
-	printf("Informe o segundo número: ");
-	fflush(stdout);
-	scanf("%d", &num2);
+	printf ("Informe o segundo número: ");
+	fflush (stdout);
+	scanf ("%d", &num2);
 
 	//Processamento
-	soma = num1 + num2;
-	multiplicacao = soma * num1;
+	soma = (num1 + num2);
 
 	//Saída
-	printf("\nO resultado da multiplicação é: ", multiplicacao);
+	printf ("A soma é %d", soma);
+
 }
